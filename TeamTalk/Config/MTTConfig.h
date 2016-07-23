871d26d36965b6158534c63a3f4d1d32a64bd106
vonway <haisafish@139.com>
2016-07-23 23:59:25 +0800

    set server ip to 192.168.1.111 for debug

--- TeamTalk/Config/MTTConfig.h
+++ TeamTalk/Config/MTTConfig.h
@@ -28,6 +28,6 @@
 #define IM_PDU_HEADER_LEN   16
 #define IM_PDU_VERSION      13
 
-#define SERVER_ADDR                             @"http://61.153.100.221:8080/msg_server"
+#define SERVER_ADDR                             @"http://192.168.1.111:8080/msg_server"
 
 #define _(x)                                NSLocalizedString(x,@"")
